f8064af9a7d7b8135f3c2677abc7b403fb39ea54
ALRICEE <alivia.rice.pht@gmail.com>
2024-07-04 10:21:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.text());
 app.use(cors());
 
 let counters = {
-    "g-": 128435,
+    "g-": 128378,
     "s-": 14444
 };
 
@@ -51,7 +51,7 @@ function processQueue() {
     counters = { ...startCounters }; // Reset counters to start from the first unreceived counter
 }
 
-setInterval(processQueue, 35000); // Process queue every 1 minute
+setInterval(processQueue, 135000); // Process queue every 1 minute
 
 app.get('/', (req, res) => {
     let response;
