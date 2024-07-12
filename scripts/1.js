f03c424f6740b2316f4445538b7e2cf54fddecb2
ALRICEE <alivia.rice.pht@gmail.com>
2024-07-12 13:20:51 +0200

    Update 1.js

--- scripts/1.js
+++ scripts/1.js
@@ -1,7 +1,6 @@
 const links = [
 "https://hugebonusfinder.life/?u=z34zyqu&o=thw8x3z&m=1&t=babe_ms",
 "https://theclicklove.top/?u=z34zyqu&o=th08x3z&m=1&t=babe_date"
-] 
-      ];
+];
       const randomLink = links[Math.floor(Math.random() * links.length)];
       window.location.href = randomLink;
